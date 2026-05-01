0369083bb80236861541448666696e25d37be5b3
drewds3 <andriudark777@gmail.com>
2026-05-01 00:15:03 -0400

    Implementación de supabase

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1>Mi primera web 🚀</h1>
-      <p>Ernesto, The Witcher 3 y Hunter x Hunter no sirven</p>
+      <p>Mírate One Piece, crak!</p>
     </div>
   );
 }
